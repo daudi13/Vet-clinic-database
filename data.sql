e9869e3345bcdafe980869d6cc460cd14bf8439d
daudi13 <davouma13@gmail.com>
2022-05-03 12:50:55 +0300

    Add insert Gabumon

--- data.sql
+++ data.sql
@@ -1,17 +1,23 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals(
-name, 
-date_of_birth, 
-escape_attempts, 
-neutered, weight_kg
-) 
-VALUES(
-'Agumon', 
-'February 3, 2020', 
-0, 
-TRUE, 
-10.23
-);
+INSERT INTO
+  animals(
+    name, 
+    date_of_birth, 
+    escape_attempts, 
+    neutered, weight_kg
+    )
+VALUES('Agumon', 'February 3, 2020', 0, TRUE, 10.23);
+
+INSERT INTO
+  animals(
+    name, 
+    date_of_birth, 
+    escape_attempts, 
+    neutered, weight_kg
+    ) 
+    VALUES('Gabumon', 'November 15, 2018', 2, TRUE, 8);
+
+
 INSERT INTO animals (name) VALUES ('Daisy');
 INSERT INTO animals (name) VALUES ('Charlie');
