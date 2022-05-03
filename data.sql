08a23e204fd841f3423e4f70baf5b661b082eb76
daudi13 <davouma13@gmail.com>
2022-05-03 18:32:00 +0300

    Add data for Ditto

--- data.sql
+++ data.sql
@@ -98,3 +98,13 @@ INSERT INTO
     escape_attempts, 
     neutered, weight_kg
     ) VALUES('Blossom', 'October 13, 1998', 3, TRUE, 17);
+
+/*Inserr data for Ditto*/
+
+INSERT INTO
+  animals(
+    name, 
+    date_of_birth, 
+    escape_attempts, 
+    neutered, weight_kg
+    ) VALUES('Ditto', 'May 14, 2022', 4, TRUE, 22);
