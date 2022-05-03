6a590cda3c8e161fb00bbf01577adec00d87127e
daudi13 <davouma13@gmail.com>
2022-05-03 12:45:13 +0300

    Add data for Agumon

--- data.sql
+++ data.sql
@@ -1,5 +1,17 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
+INSERT INTO animals(
+name, 
+date_of_birth, 
+escape_attempts, 
+neutered, weight_kg
+) 
+VALUES(
+'Agumon', 
+'February 3, 2020', 
+0, 
+TRUE, 
+10.23
+);
 INSERT INTO animals (name) VALUES ('Daisy');
 INSERT INTO animals (name) VALUES ('Charlie');
