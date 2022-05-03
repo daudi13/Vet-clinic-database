f686a88136bac239df4e31e5b9514839ee13cb32
daudi13 <davouma13@gmail.com>
2022-05-03 13:53:39 +0300

    Add List of name of all animals born between 2016 and 2019

--- queries.sql
+++ queries.sql
@@ -1,3 +1,9 @@
 /*Queries that provide answers to the questions from all projects.*/
 
+/* Find all animals whose name end in "mon" */
 SELECT * FROM animals WHERE name LIKE '%mon';
+
+/* List the nam of all animals born between 2016 and 2019 */
+
+SELECT * FROM animals WHERE date_of_birth BETWEEN 'January 1, 2016' AND 'December 31, 2019';
+
