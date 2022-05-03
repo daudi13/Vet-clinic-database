6a16d14da3a527fa2f2080357ba5cf92f9493edb
daudi13 <davouma13@gmail.com>
2022-05-03 15:02:44 +0300

    List all animals that are neutred

--- queries.sql
+++ queries.sql
@@ -18,3 +18,8 @@ SELECT date_of_birth FROM animals WHERE NAME IN ('Agumon', 'Gabumon');
 /*List name and escape attempts of animals that weigh more than  10.5kg*/
 
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+
+/*Find all animals that are neutered*/
+
+SELECT * FROM animals WHERE neutered = TRUE;
+
